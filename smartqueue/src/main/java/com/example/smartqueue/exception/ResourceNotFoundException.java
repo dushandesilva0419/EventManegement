e66f5e46ac1e710c05d24327507da2cb2e3e1491
dushandesilva0419 <dushandesilva0419@gmail.com>
2026-02-14 18:09:04 +0530

    postman doc

--- smartqueue/src/main/java/com/example/smartqueue/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartqueue.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
